675a58c960ba45146e9eb80a6bddca2321d5eb47
Business Books <95017063+business-books@users.noreply.github.com>
2021-11-30 13:54:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="youtubesearchpython",
+    name="q1youtubesearch",
     version="1.3.2",
     author="Hitesh Kumar Saini",
     author_email="saini123hitesh@gmail.com",
     description="Search for YouTube videos WITHOUT YouTube Data API v3",
     long_description= long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alexmercerind/youtube-search-python",
+    url="https://github.com/business-books/q1youtubesearch",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
